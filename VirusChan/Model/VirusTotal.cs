8cb173d716272fc8befd2a05c56fe4b2a0eb2623
chanos-dev <chanos.dev@gmail.com>
2021-01-13 03:45:11 +0900

    VirusChan -GetMD5, SHA256 메서드 추가

--- VirusChan/Model/VirusTotal.cs
+++ VirusChan/Model/VirusTotal.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,26 @@ namespace VirusChan.Model
 {
     class VirusTotal
     { 
-        private void Foo()
+        public static string GetMD5(string filePath)
         {
-             
+            using (MD5 md5 = MD5.Create())
+            {
+                using (Stream stream = File.OpenRead(filePath))
+                {                      
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-","");
+                }
+            }
+        }
+
+        public static string GetSHA256(string filePath)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                using (Stream stream = File.OpenRead(filePath))
+                {
+                    return BitConverter.ToString(sha256.ComputeHash(stream)).Replace("-", "");
+                }
+            }
         }
     }
 }
