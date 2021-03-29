925f89e033c8ebed9c4495b5d1abc8e5651bbdcc
chanos-dev <chanos.dev@gmail.com>
2021-03-30 01:35:46 +0900

    #21-0330-01
    update : formUrls 디자인

--- VirusChan/form/FormUrls.cs
+++ VirusChan/form/FormUrls.cs
@@ -26,14 +26,56 @@ namespace VirusChan.form
             if (string.IsNullOrEmpty(txt_Urls.Text))
                 return;
 
+            UrlScan urlScan;
             Program.logger.Info("URL 스캔 시작");
 
             Task.Factory.StartNew(() =>
             {
-                UrlScan urlScan = ApiController.UrlReport(txt_Urls.Text);
+                urlScan = ApiController.UrlReport(txt_Urls.Text); 
+                Program.logger.Info($"{txt_Urls.Text} URL 스캔 시작");
 
-                
+                ChangeText(lb_msg, urlScan.verbose_msg);
+                ChangeText(lb_scanDate, urlScan.scan_date);
+
+                if (urlScan.response_code == 1)
+                    ChangeEnabled(btn_detail, true); 
+                else
+                    ChangeEnabled(btn_detail, false);
+
+                Program.logger.Info($"{txt_Urls.Text} URL 스캔 완료");
             });
+        }
+
+        private void ChangeText(Control control, string text)
+        {
+            if(control.InvokeRequired)
+            {
+                control.Invoke(new Action(() => ChangeText(control, text)));
+            }
+            else
+            {
+                control.Text = text;
+            }
+        }
+
+        private void ChangeEnabled(Control control, bool flag)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new Action(() => ChangeEnabled(control, flag)));
+            }
+            else
+            {
+                control.Enabled = flag;
+            }
+        }
+
+        private void btn_detail_Click(object sender, EventArgs e)
+        {
+            using (FormFileScanDetail formFileScanDetail = new FormFileScanDetail(null))
+            {
+                formFileScanDetail.ShowDialog();
+            }
         }
     }
 }
